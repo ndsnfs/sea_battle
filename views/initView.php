9458fe06c81b27ed9b323c4e1b2c02254bacc386
nds <you@example.com>
2018-11-19 17:07:37 +0400

    Мелкие правки

---
+++ views/initView.php
@@ -0,0 +1,34 @@
+<style type="text/css">
+
+	.td {
+		display: inline-block;
+		box-sizing: border-box;
+		width: 25px;
+		height: 25px;
+		text-align: center;
+	}
+
+</style>
+<h2>Игрок №<?= count($players) + 1 ?> вводи свои данные!</h2>
+<form action="?page=init" method="POST">
+	<div>
+		<label>Имя</label>
+		<input type="text" name="player_name">
+	</div>
+	<div class="tr">
+		<div class="td"></div>
+		<?php foreach (range(0, 9) as $x): ?>
+		<div class="td"><?= $x ?></div>
+		<?php endforeach; ?>
+	</div>
+	<?php foreach (range(0, 9) as $y): ?>
+	<div class="tr">
+		<div class="td"><?= $y ?></div>
+		<?php foreach (range(0, 9) as $x): ?>
+		<div class="td"><input type="checkbox" name="cell_status[<?= $x ?>:<?= $y ?>]" value="2"></div>
+		<?php endforeach; ?>
+	</div>
+	<?php endforeach; ?>
+	<?php // (new StateWidget())->draw() ?>
+	<input type="submit" value="сохранить" name="init">
+</form>
---
+++ views/stepView.php
@@ -0,0 +1,114 @@
+<h3>Ход делает - <?= $player->getName() ?></h3>
+<style type="text/css">
+	input {
+		margin: 0;
+		padding: 0;
+	}
+	input[type="radio"] {
+		margin-top: 5px;
+	}
+	.td {
+		display: inline-block;
+		margin-bottom: 2px;
+		box-sizing: border-box;
+		width: 25px;
+		height: 25px;
+		text-align: center;
+		border: 1px solid gray;
+		vertical-align: middle;
+	}
+
+	.field {
+		float: left;
+		margin-right: 20px;
+		margin-bottom: 10px;
+	}
+	.bg-checked {
+		background-color: gray
+	}
+	.bg-danger {
+		background-color: red;
+	}
+
+</style>
+
+<form action="?page=step" method="POST">
+	<?php foreach ($fields as $field): ?>
+		<?php if($player->getId() === $field['player_id']): ?>
+		<?php
+
+		$cnt = 0;
+
+		?>
+		<div class="field">
+			<!-- первая строка с названиями столбцов -->
+			<div class="tr">
+				<div class="td"></div>
+				<?php for ($i = $minCoordinat; $i <= $maxCoordinat; $i++): ?>
+				<div class="td"><?= $i ?></div>
+				<?php endfor ?>
+			</div>
+			<!-- первая строка с названиями столбцов END -->
+			<?php foreach ($field['field_state'] as $row): ?>
+			<div class="tr">
+				<div class="td"><?= $cnt ?></div>
+				<?php foreach ($row as $k => $cellValue): ?>
+					<?php if($cellValue === 1): ?>
+					<div class="td"></div>
+					<?php elseif($cellValue === 2): ?>
+					<div class="td bg-checked"></div>
+					<?php elseif($cellValue === 3): ?>
+					<div class="td">.</div>
+					<?php elseif($cellValue === 4): ?>
+					<div class="td bg-checked">x</div>
+					<?php endif ?>
+				<?php endforeach; ?>
+				<?php $cnt++; ?>
+			</div>
+			<?php endforeach; ?>
+		</div>
+		<?php else: ?>
+		<?php
+
+		$cnt = 0;
+
+		?>
+		<div class="field">
+			<!-- первая строка с названиями столбцов -->
+			<div class="tr">
+				<div class="td"></div>
+				<?php for ($i = $minCoordinat; $i <= $maxCoordinat; $i++): ?>
+				<div class="td"><?= $i ?></div>
+				<?php endfor ?>
+			</div>
+			<!-- первая строка с названиями столбцов END -->
+			<?php foreach ($field['field_state'] as $row): ?>
+			<!-- TR -->
+			<div class="tr">
+				<div class="td"><?= $cnt ?></div>
+				<?php foreach ($row as $k => $cellValue): ?>
+					<?php if($cellValue === 1): ?>
+					<div class="td"><input type="radio" name="cell" value="<?= $k ?>"></div>
+					<?php elseif($cellValue === 2): ?>
+					<div class="td"><input type="radio" name="cell" value="<?= $k ?>"></div>
+					<?php elseif($cellValue === 3): ?>
+					<div class="td">.</div>
+					<?php elseif($cellValue === 4): ?>
+					<div class="td">x</div>
+					<?php endif ?>
+				<?php endforeach; ?>
+				<?php $cnt++; ?>
+			</div>
+			<!-- TR END -->
+			<?php endforeach; ?>
+			<input type="hidden" name="enemy_player_id" value="<?= $field['player_id'] ?>">
+			<?php // (new StateWidget())->draw() ?>
+			<input type="submit" value="огонь!" name="init" class="btn btn-block">
+		</div>
+		<?php endif ?>
+	<?php endforeach; ?>
+	<input type="hidden" name="current_player_id" value="<?= $player->getId() ?>">
+</form>
+<div style="clear: both;">
+	<a href="?page=reset">exit</a>
+</div>
